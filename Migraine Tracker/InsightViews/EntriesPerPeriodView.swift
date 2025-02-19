1cdb46db40b04bc80cee0bf5c1fb77f4d1ee96af
Holden Casey <me@holdencasey.com>
2025-02-18 21:21:57 -0500

    style for entries per period view, still have work to do

--- Migraine Tracker/InsightViews/EntriesPerPeriodView.swift
+++ Migraine Tracker/InsightViews/EntriesPerPeriodView.swift
@@ -23,27 +23,52 @@ struct EntriesPerPeriodView: View {
             }
             .pickerStyle(SegmentedPickerStyle())
             .padding()
-
+            // Add sort (descending or ascending, date or frequency)
             ScrollView {
                 VStack(alignment: .leading, spacing: 10) {
+                    // Try to make more similar to EntryList
                     ForEach(groupedEntries, id: \.0) { (period, count) in
                         HStack {
-                            Text(period)
-                                .font(.headline)
+                            // Say name of month year
+                            // Week: "2/1/25 - 2/8/25"
+                            //                            Text(period)
+                            Text("December 2025")
+                                .font(Font.custom("Avenir", size: Constants.subtitleFontSize))
+                                .multilineTextAlignment(.center)
+                                .fontWeight(.bold)
+                                .minimumScaleFactor(0.8)
+                                .lineLimit(2)
+                                .allowsTightening(true)
                             Spacer()
-                            Text("\(count)")
-                                .font(.subheadline)
+                            Text("\(count) Migraines")
+                                .font(Font.custom("Avenir", size: Constants.subtitleFontSize))
+                                .multilineTextAlignment(.center)
+                                .minimumScaleFactor(0.8)
+                                .lineLimit(2)
+                                .allowsTightening(true)
+                            Image(systemName: "chevron.right")
                                 .foregroundColor(.gray)
+                            // If clicked, goes to EntryList of those migraines
                         }
+                        .padding()
+                        .background(in: RoundedRectangle(cornerRadius: 10))
                         .padding(.horizontal)
                     }
                 }
                 .padding(.top)
             }
         }
+        .background(Color("FirstLightPink"))
     }
 }
 
 #Preview {
-    EntriesPerPeriodView(entries: [])
+    let entries: [Entry] = [
+        Entry(timestamp: Date().addingTimeInterval(-86400 * 5), intensity: 3, notes: ""), // 5 days ago
+        Entry(timestamp: Date().addingTimeInterval(-86400 * 10), intensity: 5, triggers: [], symptoms: [], treatments: [], notes: ""),
+        Entry(timestamp: Date().addingTimeInterval(-86400 * 15), intensity: 2, notes: ""),
+        Entry(timestamp: Date().addingTimeInterval(-86400 * 20), intensity: 4, notes: ""),
+        Entry(timestamp: Date().addingTimeInterval(-86400 * 25), intensity: 1, notes: "")
+    ]
+    EntriesPerPeriodView(entries: entries)
 }
